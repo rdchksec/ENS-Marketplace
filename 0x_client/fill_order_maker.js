45a6913c0abb927b1e51b71a02ba583349cfcadf
Rajesh Subhankar <bunty2327.cse@gmail.com>
2019-03-09 21:51:37 +0100

    Add ERC721

--- 0x_client/fill_order_maker.js
+++ 0x_client/fill_order_maker.js
@@ -31,29 +31,45 @@ providerEngine.start();
     const contractWrappers = new all_0x.ContractWrappers(providerEngine, { networkId: constants.GANACHE_NETWORK_ID});
     //Get Maker address
     const maker = await web3Wrapper.getAvailableAddressesAsync();
-    console.log('Maker Address:', maker);
+    console.log('Maker Address:', maker[0]);
 
     // Token Addresses------------------------------------
     const contractAddresses = deployed_contract_address_0x.getContractAddressesForNetworkOrThrow(constants.GANACHE_NETWORK_ID);
     const zrxTokenAddress = contractAddresses.zrxToken;
     const etherTokenAddress = contractAddresses.etherToken;
+    const erc721TokenAddress = '0x07f96aa816c1f244cbc6ef114bb2b023ba54a2eb'; // In ganache
     const DECIMALS = 18;
 
+    // Generate TokenID
+    // Generate a random token id
+    const tokenId = 1;//all_0x.generatePseudoRandomSalt();
+
     //Create asset data--------------------------------------
-    const makerAssetData = all_0x.assetDataUtils.encodeERC20AssetData(zrxTokenAddress);
+    //const makerAssetData = all_0x.assetDataUtils.encodeERC20AssetData(zrxTokenAddress);
+    const makerAssetData = all_0x.assetDataUtils.encodeERC721AssetData(erc721TokenAddress, tokenId);
     const takerAssetData = all_0x.assetDataUtils.encodeERC20AssetData(etherTokenAddress);
     // the amount the maker is selling of maker asset (future: 1 ENSNFT)
-    const makerAssetAmount = web3Wrapper_0x.Web3Wrapper.toBaseUnitAmount(new all_0x.BigNumber(1), DECIMALS);
-    // the amount the maker wants of taker asset (Future: 10 Dai)
-    const takerAssetAmount = web3Wrapper_0x.Web3Wrapper.toBaseUnitAmount(new all_0x.BigNumber(10), DECIMALS);
+    //const makerAssetAmount = web3Wrapper_0x.Web3Wrapper.toBaseUnitAmount(new all_0x.BigNumber(1), DECIMALS);
+    const makerAssetAmount = new all_0x.BigNumber(1);
+    // the amount the maker wants of taker asset (Now: 0.1 eth, Future: 10 Dai)
+    const takerAssetAmount = web3Wrapper_0x.Web3Wrapper.toBaseUnitAmount(new all_0x.BigNumber(0.1), DECIMALS);
+
 
     //Set Allowances-------------------------------------------
     // Allow the 0x ERC20 Proxy to move ZRX on behalf of makerAccount
-    const makerZRXApprovalTxHash = await contractWrappers.erc20Token.setUnlimitedProxyAllowanceAsync(
+    /*const makerZRXApprovalTxHash = await contractWrappers.erc20Token.setUnlimitedProxyAllowanceAsync(
         zrxTokenAddress,
         maker[0],
     );
-    await web3Wrapper.awaitTransactionSuccessAsync(makerZRXApprovalTxHash);
+    await web3Wrapper.awaitTransactionSuccessAsync(makerZRXApprovalTxHash);*/
+    // Allow the 0x ERC721 Proxy to move ERC721 tokens on behalf of maker
+    const makerERC721ApprovalTxHash = await contractWrappers.erc721Token.setProxyApprovalForAllAsync(
+        erc721TokenAddress,
+        maker[0],
+        true,
+    );
+    await web3Wrapper.awaitTransactionSuccessAsync(makerERC721ApprovalTxHash);
+    console.log('Maker ERC721 Approhal TxHash: ', makerERC721ApprovalTxHash);
 
     //Create Order--------------------------------------------------------------
     // Set up the Order and fill it
